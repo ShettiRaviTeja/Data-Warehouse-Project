babe3ecb5a937f51b9339b92ee47744d00ed253c
ShettiRaviTeja <159680650+ShettiRaviTeja@users.noreply.github.com>
2026-06-22 07:38:37 +0530

    Refactor SQL insert logic for crm_sales_details

--- scripts/silver layer/sp_load_silver.sql
+++ scripts/silver layer/sp_load_silver.sql
@@ -107,38 +107,56 @@ BEGIN
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating the table: silver.crm_sales_details';
 		TRUNCATE TABLE silver.crm_sales_details;
-		PRINT '>> Inserting into table: silver.crm_sales_details'
-		INSERT INTO silver.crm_sales_details
+		PRINT '>> Inserting into table: silver.crm_sales_details';
+		WITH crm_sales AS
 		(
+		SELECT 
+					sls_ord_num,
+					sls_prd_key,
+					sls_cust_id,
+					TRY_CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) sls_order_dt, -- Casting the INT into DATE
+					TRY_CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) sls_ship_dt,
+					TRY_CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) sls_due_dt,
+					CASE 
+						WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales <> sls_quantity * ABS(sls_price)
+							THEN sls_quantity * ABS(sls_price)
+						ELSE sls_sales
+					END sls_sales, -- Handling the NULLs, Zeroes and Wrong Calculations in Sales
+					sls_quantity,
+					CASE 
+						WHEN sls_price IS NULL OR sls_price <= 0 
+							THEN sls_sales / NULLIF(sls_quantity, 0) 
+						ELSE sls_price
+					END sls_price -- Handling the NULLs, Zeroes, negative values in Price
+				FROM bronze.crm_sales_details
+		)
+		INSERT INTO silver.crm_sales_details
+				(
+					sls_ord_num,
+					sls_prd_key,
+					sls_cust_id,
+					sls_order_dt,
+					sls_ship_dt,
+					sls_due_dt,
+					sls_sales,
+					sls_quantity,
+					sls_price
+				)
+		SELECT 
 			sls_ord_num,
 			sls_prd_key,
 			sls_cust_id,
-			sls_order_dt,
+			CASE 
+				WHEN sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+					THEN NULL
+				ELSE sls_order_dt
+			END sls_order_dt, -- Handling invalid dates
 			sls_ship_dt,
 			sls_due_dt,
 			sls_sales,
 			sls_quantity,
 			sls_price
-		)
-		SELECT 
-			sls_ord_num,
-			sls_prd_key,
-			sls_cust_id,
-			TRY_CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) sls_order_dt, -- Casting the INT into DATE
-			TRY_CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) sls_ship_dt,
-			TRY_CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) sls_due_dt,
-			CASE 
-				WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales <> sls_quantity * ABS(sls_price)
-					THEN sls_quantity * ABS(sls_price)
-				ELSE sls_sales
-			END sls_sales, -- Handling the NULLs, Zeroes and Wrong Calculations in Sales
-			sls_quantity,
-			CASE 
-				WHEN sls_price IS NULL OR sls_price <= 0 
-					THEN sls_sales / NULLIF(sls_quantity, 0) 
-				ELSE sls_price
-			END sls_price -- Handling the NULLs, Zeroes, negative values in Price
-		FROM bronze.crm_sales_details;
+		FROM crm_sales;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS VARCHAR) + ' Seconds'
 		PRINT '---------------------------'
