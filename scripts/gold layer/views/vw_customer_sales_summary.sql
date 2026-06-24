fb749a5535fd11fab0c903e056c9a6bf52a2a81b
ShettiRaviTeja <159680650+ShettiRaviTeja@users.noreply.github.com>
2026-06-24 18:17:39 +0530

    Refactor vw_customer_sales_summary view definition

--- scripts/gold layer/views/vw_customer_sales_summary.sql
+++ scripts/gold layer/views/vw_customer_sales_summary.sql
@@ -23,30 +23,30 @@ Key Metrics:
 - Last Purchase Date
 - Customer Lifespan
 */
-
-SELECT 
-	c.customer_id,
-	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
-	c.country,
-	c.marital_status,
-	c.gender,
-	DATEDIFF(YEAR, c.birthdate, GETDATE()) AS age,
-	c.create_date,
-	SUM(f.sales_amount) AS total_sales,
-	COUNT(DISTINCT f.order_number) AS total_orders,
-	SUM(f.quantity) AS total_quantity,
-	CAST(SUM(f.sales_amount) * 1.0 / COUNT(DISTINCT f.order_number) AS DECIMAL(18,2)) AS average_order_value,
-	MIN(f.order_date) AS first_purchase_date,
-	MAX(f.order_date) AS last_purchase_date,
-	DATEDIFF(DAY, MIN(f.order_date), MAX(f.order_date)) AS customer_lifespan
-FROM gold.fact_sales AS f
-INNER JOIN gold.dim_customers AS c
-ON f.customer_key = c.customer_key
-GROUP BY 
+CREATE VIEW vw_customer_sales_summary AS
+	SELECT 
 		c.customer_id,
-		CONCAT(c.first_name, ' ', c.last_name),
+		CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
 		c.country,
 		c.marital_status,
 		c.gender,
-		c.birthdate, 
-		c.create_date;
+		DATEDIFF(YEAR, c.birthdate, GETDATE()) AS age,
+		c.create_date,
+		SUM(f.sales_amount) AS total_sales,
+		COUNT(DISTINCT f.order_number) AS total_orders,
+		SUM(f.quantity) AS total_quantity,
+		CAST(SUM(f.sales_amount) * 1.0 / COUNT(DISTINCT f.order_number) AS DECIMAL(18,2)) AS average_order_value,
+		MIN(f.order_date) AS first_purchase_date,
+		MAX(f.order_date) AS last_purchase_date,
+		DATEDIFF(DAY, MIN(f.order_date), MAX(f.order_date)) AS customer_lifespan
+	FROM gold.fact_sales AS f
+	INNER JOIN gold.dim_customers AS c
+	ON f.customer_key = c.customer_key
+	GROUP BY 
+			c.customer_id,
+			CONCAT(c.first_name, ' ', c.last_name),
+			c.country,
+			c.marital_status,
+			c.gender,
+			c.birthdate, 
+			c.create_date;
